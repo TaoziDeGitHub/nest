783033519e4960c2d82f299a3ff077a63dcdfc1e
huangtaotao <1520887952@qq.com>
2021-12-29 14:48:27 +0800

    一些修改

--- dist/app.service.js
+++ dist/app.service.js
@@ -10,7 +10,7 @@ exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
 let AppService = class AppService {
     getHello() {
-        return 'Hello World!';
+        return '如果你看到这个，就是成功了！';
     }
 };
 AppService = __decorate([
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '如果你看到这个，就是成功了！';
   }
 }
